e3d3e2a90b42877ca6f330fbeb1959ac6cb2e57c
ahmadtech199 <ahmadtech199@gmail.com>
2020-04-05 13:30:49 +0300

    Add existing file

--- Order.py
+++ Order.py
@@ -1,6 +1,10 @@
 class Order:
     _id = 0
-    def __init__(self,date,is_paid,person,product):
-        self.date= date
+
+    def __init__(self, date, is_paid, person, products):
+        Order._id += 1
+        self.id = Order._id
+        self.date = date
         self.is_paid = is_paid
-        self.person
+        self.person = person
+        self.products = products
